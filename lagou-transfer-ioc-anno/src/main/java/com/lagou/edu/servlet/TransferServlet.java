f3031b01add23ff98efc90d9e8a8937b7d288036
CHYGO1985 <jingjiejiang85@gmail.com>
2021-06-06 18:43:31 +0800

    Able to transfer via Transactional anno

--- lagou-transfer-ioc-anno/src/main/java/com/lagou/edu/servlet/TransferServlet.java
+++ lagou-transfer-ioc-anno/src/main/java/com/lagou/edu/servlet/TransferServlet.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,8 +39,15 @@ public class TransferServlet extends HttpServlet {
 //    }
 
     @Autowired
-    @Qualifier("transferService")
     private TransferService transferService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
+                config.getServletContext());
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
