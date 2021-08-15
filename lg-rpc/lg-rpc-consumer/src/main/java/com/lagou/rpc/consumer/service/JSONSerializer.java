a09d51155a54b5083ac7be29a98c7053e744b5b9
CHYGO1985 <jingjiejiang85@gmail.com>
2021-08-15 17:44:53 +0800

    Add serializer interface and its JSON implementation

---
+++ lg-rpc/lg-rpc-consumer/src/main/java/com/lagou/rpc/consumer/service/JSONSerializer.java
@@ -0,0 +1,27 @@
+package com.lagou.rpc.consumer.service;
+
+import com.alibaba.fastjson.JSON;
+import com.lagou.rpc.api.Serializer;
+
+import java.io.IOException;
+
+/**
+ *
+ * The JSON implementation of Inteface Serializer.
+ *
+ * @author jingjiejiang
+ * @history Aug 15, 2021
+ *
+ */
+public class JSONSerializer implements Serializer {
+
+    @Override
+    public byte[] serialize(Object object) throws IOException {
+        return JSON.toJSONBytes(object);
+    }
+
+    @Override
+    public <T> T deserialize(Class<T> targetClass, byte[] bytes) throws IOException {
+        return JSON.parseObject(bytes, targetClass);
+    }
+}
